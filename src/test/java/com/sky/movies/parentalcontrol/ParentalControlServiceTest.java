a66b9c97671623e980f13922e9064120c9e85771
Sergej Bobusky <bobuskysergej@gmail.com>
2018-12-18 05:09:04 +0000

    ParentalControlService implemented

--- src/test/java/com/sky/movies/parentalcontrol/ParentalControlServiceTest.java
+++ src/test/java/com/sky/movies/parentalcontrol/ParentalControlServiceTest.java
@@ -31,6 +31,29 @@ public class ParentalControlServiceTest {
         when(movieService.getParentalControlLevel(movieId)).thenReturn("18");
         boolean canWatch = service.canWatchMovie(ControlLevel.AGE_18, movieId);
         assertThat(canWatch).isTrue();
+    }
 
+    @Test
+    public void cannotWatchMovie() throws TechnicalFailureException, TitleNotFoundException, ParentalControlServiceException {
+        String movieId = "aquaman";
+        when(movieService.getParentalControlLevel(movieId)).thenReturn("18");
+        boolean canWatch = service.canWatchMovie(ControlLevel.AGE_15, movieId);
+        assertThat(canWatch).isFalse();
+    }
+
+    @Test
+    public void cannotWatchMovieTechnicalFailure() throws TechnicalFailureException, TitleNotFoundException, ParentalControlServiceException {
+        String movieId = "aquaman";
+        when(movieService.getParentalControlLevel(movieId)).thenThrow(TechnicalFailureException.class);
+        boolean canWatch = service.canWatchMovie(ControlLevel.AGE_15, movieId);
+        assertThat(canWatch).isFalse();
+    }
+
+    @Test(expected = ParentalControlServiceException.class)
+    public void parentalControlServiceException() throws TechnicalFailureException, TitleNotFoundException, ParentalControlServiceException {
+        String movieId = "batman";
+        when(movieService.getParentalControlLevel(movieId)).thenThrow(TitleNotFoundException.class);
+        boolean canWatch = service.canWatchMovie(ControlLevel.AGE_18, movieId);
+        assertThat(canWatch).isTrue();
     }
 }
